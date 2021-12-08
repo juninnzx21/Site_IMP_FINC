ab77a27a5fa62a6afd410295faaf328ff803498b
geraldosd <juninhosh2013sh@gmail.com>
2021-12-08 17:11:41 -0300

    logo

--- includes/head.php
+++ includes/head.php
@@ -6,6 +6,7 @@ include 'time_config.php';
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="shortcut icon" href="<?= base_url();?>assets/images/l.png" type="image/x-icon" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous" />
@@ -13,7 +14,7 @@ include 'time_config.php';
     <link rel="stylesheet" href="<?=base_url()?>assets/css/font-awesome.min.css">
     <title>MulticredBH</title>
     <meta charset="utf-8">
-    <meta name="keywords" content="Uma empresa focada em ajudar os crientes do INSS">
+    <meta name="keywords" content="Uma empresa focada em ajudar os clientes do INSS">
     <meta name="author" content="Junior Rodrigues">
     <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
