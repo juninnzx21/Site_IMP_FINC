6c2d421641a62f892a8685f7e144843c4d383721
geraldosd <juninhosh2013sh@gmail.com>
2021-12-16 16:03:46 -0300

    are

--- pages/sobre.php
+++ pages/sobre.php
@@ -53,7 +53,67 @@
 </div>
 
 
+<footer class="text-light p-3 bg-dark animate__animated animate__fadeInBottomLeft">
+    <div class="row mt-4 m-auto">
+        <div class="row">
+            <div class="col-xl-3 col-sm-12 box-footer ">
+                <h5 class="text-center">Endereço</h5>
+                <p class="text-center">Rua Conceição Macedo de Novais, 140 - Cidade Nova, Belo Horizonte-MG, 31170160,
+                    Brasil</p>
+                <p class="text-center">Av. Amador Bueno da Veiga, 2032 - Penha de França, São Paulo - SP, 03636-100,
+                    Brasil</p>
+            </div>
+            <div class="col-xl-3 col-sm-12 box-footer">
+                <h5 class="text-center">Telefone</h5>
+                <p class="text-center">(31)2333-0152</p>
+            </div>
+            <div class="col-xl-3 col-sm-12 box-footer">
+                <div>
+                    <h6 class="text-center">Whatsapp da unidade de Belo Horizonte</h6>
+                    <p class="text-center">(31) 9 91739320</p>
+                    <p class="text-center">(31) 9 93724978</p>
+                </div>
+                <div>
+                    <h6 class="text-center">Whatsapp da unidade de São Paulo</h6>
+                    <p class="text-center">(11) 9 60736160</p>
+                    <p class="text-center">(11) 9 34917912</p>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-12 box-footer">
 
+                <div class="text-center">
+                    <font color='white'>Converse com um de nossos atendentes nas redes sociais a seguir!</font>
+                    <a href='https://www.facebook.com/multicredbh'><img src="<?=base_url()?>assets/images/facebook.png"
+                            style="max-height: 25px"></a>
+                    <a href='https://www.instagram.com/multicredbh/'><img
+                            src="<?=base_url()?>assets/images/instagram.png" alt="" class="img-fluid"
+                            style="max-height: 25px"></a>
+                    <a
+                        href='https://api.whatsapp.com/send?phone=553191739320&text=Ol%C3%A1%20MulticredBH%20vim%20pelo%20site%2C%20quero%20adquirir%20meu%20orçamento%20agora!'><img
+                            src="<?=base_url()?>assets/images/whatsapp.png" alt="" class="img-fluid"
+                            style="max-height: 25px"></a>
+
+                </div>
+
+                <a class="nav-link text-center text-white" href="<?=$url?>pages/atendimento-ao-cliente.php">Atendimento
+                    ao
+                    Cliente</a>
+                <h5 class="text-center text-">Receba nossas news</h5>
+                <form method="post" action="<?=$url?>includes/process2.php">
+                    <div class="input-group mb-3">
+                        <input name='email' class="form-control" placeholder="E-mail..." type="email">
+                        <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane-o"></i></button>
+                    </div>
+
+
+                </form>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <p class="p-2 text-center">© <?= date('Y')?>. Todos os direitos reservados. Multicredbh Crédito Financiamento e
+        Investimento.</p>
+</footer>
 
 
 
