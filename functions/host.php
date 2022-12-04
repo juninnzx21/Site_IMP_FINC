88b8ded5170d18e718b3d931f05e492e3864e536
Geraldo Rodrigues de Oliveira <82852743+geraldosdsh@users.noreply.github.com>
2022-12-04 13:36:35 -0300

    ssss

--- functions/host.php
+++ functions/host.php
@@ -5,7 +5,7 @@ if ($_SERVER['SERVER_NAME'] === $_SERVER['HTTP_HOST'] && $_SERVER['HTTP_HOST'] =
     # localhost
     function base_url()
     {
-        return 'http://localhost/site/';
+        return 'https://geraldosdsh.github.io/Site_IMP_FINC/';
     }
 }else{
     # servidor
